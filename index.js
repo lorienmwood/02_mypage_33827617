bafcd53a050023c0fcd5a181cc164b2639eceda7
LorienWood <you@example.com>
2025-10-15 16:54:24 +0100

    updated js

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-var http = require("http"); 
+var http = require("http");
 
-const port = 8000; 
+const port = 8000;
 
-http.createServer(function(req, res) { 
+http
+  .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/HTML" });
     res.end(
-    `<!DOCTYPE html>
+      `<!DOCTYPE html>
 <html>
 <head>
     <title>A website about Me</title>
     <style>
-    body {background-color: darkgrey;
+    body {background-color: black;
     color: white;}
     </style>
 </head>
@@ -19,7 +20,9 @@ http.createServer(function(req, res) {
     <h2> A Computer Science Student</h2>
     <p>I study at Goldsmiths in London.</p>
 </body>
-</html>`);
-    }).listen(port, function() { 
-        console.log(`Node server is running on port ${port}...`); 
-}); 
+</html>`
+    );
+  })
+  .listen(port, function () {
+    console.log(`Node server is running on port ${port}...`);
+  });
